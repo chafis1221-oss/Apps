c50ef35c8b1d51e72549912631029da50d7df2c1
chafis1221-oss <chafis1221@gmail.com>
2025-11-30 06:05:23 +0700

    Simplify Vite config by using default React plugin

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,12 +2,5 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [
-    react({
-      jsxRuntime: 'automatic' // Pastikan pakai new JSX transform
-    })
-  ],
-  build: {
-    sourcemap: true // Untuk debug lebih mudah
-  }
+  plugins: [react()] // ← Biarkan default
 })
